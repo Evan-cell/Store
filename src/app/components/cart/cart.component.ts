f83bffb473405ed1c444c3a32ea20e13f325a56e
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-08-26 13:35:45 +0300

    added interpolation to display the items in the cart route

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from './../../service/cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
-
+  public product : any = [];
+  public grandTotal !: number;
+  constructor(private CartService : CartService) { }
   ngOnInit(): void {
+    this.CartService.getProduct()
+    .subscribe(res=>{
+      this.product = res;
+      this.grandTotal = this.CartService.getTotalprice();
+    })
+  }
+  removeItem(item:any){
+    this.CartService.removeCartItem(item)
+  }
   }
 
-}
+
